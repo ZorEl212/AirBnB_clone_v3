270839e60003b99bd257d98bcabd4a3ae0306f8c
ZorEl212 <yabsirad212@proton.me>
2024-02-09 19:38:11 +0300

    GENERAL: add docstrings and fix several pep-8 errors
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""starting pointthe web app"""
+
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
@@ -9,9 +11,11 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,9 @@
 """Index page of the web app"""
 
 from flask import jsonify
-
 from api.v1.views import app_views
+
+
 @app_views.route("/status", strict_slashes=False)
 def app_status():
     return jsonify({"status": "ok"})
