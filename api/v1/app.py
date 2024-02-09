0dd91a043dbf16065eac3e357b824e1f2e775c2f
ZorEl212 <yabsirad212@proton.me>
2024-02-09 20:22:17 +0300

    API: add error handler for 404
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """starting pointthe web app"""
 
 from models import storage
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 from api.v1 import host, port
 
@@ -17,5 +17,10 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == '__main__':
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
