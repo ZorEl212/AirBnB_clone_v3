62db3a7c52995810faac1a071540baefb33a46bd
ZorEl212 <yabsirad212@proton.me>
2024-02-09 22:57:40 +0300

    API: + to refactoring
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,4 +2,15 @@
 from flask import Blueprint
 
 app_views = Blueprint("views", __name__, url_prefix='/api/v1')
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {'states': State, 'cities': City, 'users': User,
+           'places': Place, 'reviews': Review, 'amenities': Amenity}
+
 from api.v1.views.index import *
+from api.v1.views.state import *
