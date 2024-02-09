347424e351ea30cf66ac5ee3f617f9c05510c7c3
ZorEl212 <yabsirad212@proton.me>
2024-02-09 20:00:12 +0300

    API: add endpoint for reporting status of all classes
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,18 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
+classes = {'states': 'State', 'cities': 'City', 'users': 'User',
+           'palces': 'Place', 'reviews': 'Review','amenities': 'Amenity'}
 @app_views.route("/status", strict_slashes=False)
 def app_status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    all_stats = {}
+    for key, value in classes.items():
+        all_stats[key] = storage.count(value)
+    return all_stats
